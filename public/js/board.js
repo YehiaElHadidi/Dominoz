cb7f894826dfa1f032b632fb21f29dfe9acb30e2
YehiaElHadidi <hadidiyehia@gmail.com>
2023-04-27 22:29:13 +0200

    Board

---
+++ public/js/board.js
@@ -0,0 +1,61 @@
+"use strict";
+// for uninformed.ejs
+let board = document.getElementById("board");
+let ROW;
+let COL;
+const boardInit = function (event) {
+    board.innerHTML = "";
+    // create board
+    let row = document.getElementById("rowsInput").value;
+    let col = document.getElementById("colsInput").value;
+    ROW = row;
+    COL = col;
+    for (let i = 0; i < row; i++) {
+        const tr = board.insertRow();
+        for (let j = 0; j < col; j++) {
+            const td = tr.insertCell();
+            td.className = "cell";
+            td.setAttribute("onclick", "placeBomb(this.id)")
+            td.setAttribute('id', i + "-" + j);
+            tr.appendChild(td);
+        }
+    }
+    const sub = document.getElementById("buttonSubmit");
+    if(sub.childElementCount === 0) {
+        const btn = document.createElement("button");
+        btn.className = "ms-3 btn btn-dark"
+        btn.setAttribute("onclick", "sendBoard()");
+        btn.innerHTML = "Submit Board";
+        sub.appendChild(btn);
+    }
+
+};
+
+
+function placeBomb(id) {
+    var box = document.getElementById(id);
+    console.log(id);
+    if (box.className === "cell") {
+        box.className = "bomb"
+    } else {
+        box.className = "cell";
+    }
+}
+
+function sendBoard(){
+    var arr = new Array(ROW);
+    for(let i = 0; i < ROW; i++)
+        arr[i] = new Array(COL);
+
+    for (let i = 0; i < ROW; i++){
+        for(let j = 0; j < COL; j++){
+            if(document.getElementById(i + "-" + j).className == "bomb"){
+                arr[i][j] = 'x';
+            }
+            else{
+                arr[i][j] = '#';
+            }
+        }
+    }
+    console.log(arr);
+}
--- public/js/uninformed.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// for uninformed.ejs
-const handleSubmit = function (event) {
-  event.preventDefault();
-  // create board
-};
